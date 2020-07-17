8f130585bd3313dfec8d7702f0ae2c37d52c3587
V <mrgolt>
2020-07-17 17:36:09 +0500

    update get_sales.py auto refactor

--- app/get_sales.py
+++ app/get_sales.py
@@ -1,5 +1,6 @@
-from app.mysql import con
+from app.mysql import con, get_last_stocks
 import requests
+from datetime import datetime
 import json
 
 
@@ -15,10 +16,10 @@ def get_artikuls():
     return arts
 
 
-def add_sales (artikul, orders):
+def add_sales (artikul, orders, sales):
     with con:
         cur = con.cursor()
-        query = "INSERT INTO `orders` (art_id, orders, timestamp) VALUES ('{}', '{}', NOW())".format(artikul, orders)
+        query = "INSERT INTO `orders` (art_id, orders, orders_diff,timestamp) VALUES ('{}', '{}', '{}', NOW())".format(artikul, orders, sales)
         cur.execute(query)
 
 
@@ -27,7 +28,6 @@ def main():
     arts = get_artikuls()
     for art in arts:
         artikul += str(art)+";"
-    print(len(artikul))
 
     url_api = 'https://nm-2-card.wildberries.ru/enrichment/v1/api?spp=0&pricemarginCoeff=1.0&reg=0&appType=1&offlineBonus=0&onlineBonus=0&emp=0&nm={}'.format(artikul)
     print(url_api)
@@ -43,8 +43,11 @@ def main():
                     stocks += wh['qty']
             arts[product['id']] = stocks
 
-    for key in arts.keys():
-        add_sales(key, arts[key])
+    for key in arts:
+        last_orders = get_last_stocks(key)
+        order_diff = last_orders-arts[key] if last_orders > 0 else 0
+        print(order_diff, last_orders)
+        add_sales(key, arts[key],order_diff)
 
 
 if __name__ == '__main__':
